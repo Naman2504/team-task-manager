d87125f986312bbd835ce4ab24a4ebf2d1061652
Naman2504 <bansalnaman1920@gmail.com>
2026-05-03 23:06:00 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:8081/api'
+  baseURL: 'team-task-manager-production-a0bb.up.railway.app'
 })
 
 API.interceptors.request.use((req) => {
